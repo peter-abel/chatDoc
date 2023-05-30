7b0542c82c427aea99b707711cd3e3b254f70055
Abel Peter <peterabel791@gmail.com>
2023-05-30 10:01:51 +0300

    first commit

---
+++ chat.py
@@ -0,0 +1,6 @@
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
